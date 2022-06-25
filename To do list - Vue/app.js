121770275aeae11b65e27df779a3518b8bf2ce0c
Georgi Dechev <georgiddechev@gmail.com>
2022-06-25 22:06:19 +0300

    Update app.js

--- To do list - Vue/app.js
+++ To do list - Vue/app.js
@@ -12,16 +12,3 @@ Vue.createApp({
       }
     }
   }).mount('#app');
-
-// const buttonElement = document.querySelector('button');
-// const inputElement = document.querySelector('input');
-// const listElement = document.querySelector('ol');
-// function addTask() {
-//     const taskValue = inputElement.value;
-//     const listTaskElement = document.createElement('li');
-//     listTaskElement.textContent = taskValue;
-//     listElement.appendChild(listTaskElement)
-//     inputElement.value = "";
-// }
-// buttonElement.addEventListener('click', addTask);
-
